839fcd0414b0e199d5cd3e6693dc292a4decf03e
curex <47378577+curexcode@users.noreply.github.com>
2021-03-31 10:12:59 +0530

    Initial commit.

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+   <h1>{{title}}</h1> 
+</template>
+
+<script>
+import Button from './Button';
+
+ export default {
+     name: 'Header',
+     props: {
+         title:{
+             type: String,
+             default: 'Default text'
+         }
+     }, 
+     components: {
+         Button,
+     }
+ }
+
+
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+
+</style>
