c9fb1e863abf859452e511c80260e7761510d2e7
curex <47378577+curexcode@users.noreply.github.com>
2021-03-31 13:50:10 +0530

    Add task component and style.

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,6 +1,7 @@
 <template>
     <div :key="task.id" v-for="task in tasks">
-        <h3>{{ task.text }}</h3>
+        <Task :task="task" />
+        <!-- <h3>{{ task.text }}</h3> -->
     </div>
     
 </template>
@@ -8,11 +9,16 @@
 
 <script>
 
+import Task from './Task';
+
 export default{
     name: 'Tasks',
     props: {
         tasks: Array,
     },
+    components: {
+        Task,
+    }
 }
 
 </script>
