5d07db37ab144aac492568d23a7b4ca3b876cf6e
curex <47378577+curexcode@users.noreply.github.com>
2021-04-01 18:53:23 +0530

    Add computed property to hide button on about page.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
    <header>
         <h1>{{title}}</h1> 
-        <Button @btn-click="$emit('toggle-add-task')" 
+        <Button v-show="homePage" @btn-click="$emit('toggle-add-task')" 
         :text="showAddTask ? 'Close' : 'Add Task' " 
         :color="showAddTask? 'red' : 'green'"/>
    </header>
@@ -24,7 +24,15 @@ import Button from './Button';
      components: {
          Button,
      },
-     emits: ['toggle-add-task']
+     emits: ['toggle-add-task'],
+     computed: {
+         homePage(){
+             if(this.$route.path === '/')
+                 return true;
+             else 
+                return false;
+         }
+     }
  }
 
 
